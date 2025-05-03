be94f683fba14caad6273eec53f8bf7c1a39811f
Bryan Maamo <ninbryan@users.noreply.github.com>
2025-05-02 23:29:32 -0700

    updated style to NES

---
+++ clients/home/app.js
@@ -0,0 +1,12 @@
+{
+  document.querySelectorAll('section[id]').forEach((section) => {
+    const id = section.id;
+    const h2 = section.querySelector('h2');
+
+    if (h2) {
+      h2.innerHTML = `<a href="#${id}">#</a> ` + h2.innerHTML;
+    }
+
+    section.classList.add('nes-container', 'is-rounded', 'is-dark');
+  });
+}
