95b1cdd029a9a3f8f457346957e01dd81629d918
austintownsend <austintownsend1234@gmail.com>
2025-05-17 22:11:50 -0700

    setup initial server api calls for vote route

--- clients/voter/app/routes/vote.jsx
+++ clients/voter/app/routes/vote.jsx
@@ -1,9 +1,53 @@
 import {VotePage} from "../pages/VotePage/VotePage.jsx";
 import {useNavigate} from "react-router";
 import {useEffect} from "react";
+import {api} from "../api.js";
+import {hasUniqueId} from "../utils/uniqueId.js";
+
+export const meta = () => [
+    { title: '.hack//voter' },
+    { name: 'description', content: 'page to vote on your favorite projects' }
+];
+
+export async function loader({params}) {
+    try {
+        const campaignId = params.campaignId;
+        const {campaign} = await api.getCampaign(true, campaignId)
+        const {projects} = await api.getCampaignProjects(true, campaignId);
+        return {
+            campaign: campaign || {},
+            projects: projects || []
+        };
+    } catch {
+        return {
+            campaign: {},
+            projects: []
+        };
+    }
+}
+
+export async function clientLoader({serverLoader}) {
+    const {campaign, projects} = await serverLoader();
+    const isReturningGuest = hasUniqueId();
+    return {
+        isReturningGuest,
+        campaign,
+        projects
+    };
+}
+
+clientLoader.hydrate = true;
+
+export function HydrateFallback() {
+    return (
+        <>
+            <marquee>loading...</marquee>
+        </>
+    )
+}
 
 export default function VoteRoute({loaderData}) {
-    const {campaign, projects, isReturningGuest} = loaderData;
+    const {isReturningGuest, campaign, projects} = loaderData;
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -12,15 +56,11 @@ export default function VoteRoute({loaderData}) {
         }
     }, [campaign.isActive, navigate])
 
-    if (campaign.isActive) {
-        return (
-            <VotePage
-                {...campaign}
-                projects={projects}
-                isReturningGuest={isReturningGuest}
-            />
-        );
-    } else {
-
-    }
+    return (
+        <VotePage
+            {...campaign}
+            projects={projects}
+            isReturningGuest={isReturningGuest}
+        />
+    );
 }
